f8d078ece2989ca727098f8a7da5ea4dc18a274e
Benjamin Horns <benhorns@gmail.com>
2019-12-10 23:10:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,6 +4,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
+  elseif int % 5 == 0
+    "Buzz"
+  elseis int % 5,3 == 0
+    "Fizzbuzz"
   end
 
 end
